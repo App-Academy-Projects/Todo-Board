eff7654c4e4cbac8241a4e1dcaf7d4b668542af7
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-17 22:04:21 +0200

    Check is deadline has a valid date

--- item.rb
+++ item.rb
@@ -1,7 +1,15 @@
 class Item
     attr_accessor :title, :deadline, :description
+    def self.valid_date?(date_string)
+        year, month, day = date_string.split('-')
+        valid_year = year > "1000" && year < "3000"
+        valid_month = month.to_i > 0 && month.to_i <= 12
+        valid_day = day.to_i > 0 && day.to_i <= 31
+        return valid_year && valid_month && valid_day
+    end
 
     def initialize(title, deadline, description)
+        raise "deadline: #{deadline} is Invalid Date" if !Item.valid_date?(deadline)
         @title = title
         @deadline = deadline
         @description = description
