1a9442e0ddebbaa39bb527db2f9dce328531353e
Usama Fouad <usamaeltmsah@gmail.com>
2020-09-17 22:21:26 +0200

    Add new item to the List

--- list.rb
+++ list.rb
@@ -1,7 +1,14 @@
+require './item.rb'
+
 class List
     attr_accessor :label
     def initialize(label)
         @label = label
         @items = []
+    end
+
+    def add_item(title, deadline, description)
+        return false if !Item.valid_date?(deadline)
+        return true if @items << Item.new(title, deadline, description)
     end
 end
