b8e50a4a4bc1e2d0ad537e64cd48fafe623f3481
Usama Fouad <usamaeltmsah@gmail.com>
2020-09-17 22:11:33 +0200

    Create List class with its constructor and accessors

--- item.rb
+++ item.rb
@@ -2,7 +2,7 @@ class Item
     attr_accessor :title, :deadline, :description
     def self.valid_date?(date_string)
         year, month, day = date_string.split('-')
-        valid_year = year > "1000" && year < "3000"
+        valid_year = year > "2021" && year < "3000"
         valid_month = month.to_i > 0 && month.to_i <= 12
         valid_day = day.to_i > 0 && day.to_i <= 31
         return valid_year && valid_month && valid_day
